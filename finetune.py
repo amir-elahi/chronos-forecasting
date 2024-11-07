57196dfcd48e6656d8dffe589c6f17089c427d6c
Amir remote <amir.elahi@epfl.ch>
2024-11-07 17:42:44 +0100

    Some changes, to be checked

--- finetune.py
+++ finetune.py
@@ -1,17 +1,22 @@
 import subprocess
+import os
 
 # Define the command as a list of arguments
 command = [
     "python", "scripts/training/train.py",
     "--config", "./scripts/training/configs/chronos-t5-tiny.yaml",
-    "--model-id", "amazon/chronos-t5-small",
+    "--model-id", "amazon/chronos-t5-tiny",
     "--no-random-init",
     "--max-steps", "1000",
     "--learning-rate", "0.001"
 ]
 
-# Run the command without output buffering
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, text=True)
+# Set the environment variable for CUDA
+env = os.environ.copy()
+env["CUDA_VISIBLE_DEVICES"] = "0"  # Use '0' to specify GPU 0
+
+# Run the command with the updated environment
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, text=True, env=env)
 
 # Print the output line by line in real-time
 for stdout_line in iter(process.stdout.readline, ""):
